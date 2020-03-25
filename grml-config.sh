cb4e9cabbd475bfdcf89750351e386af049a569e
Ricky Laney <rlaneyjr@gmail.com>
2020-03-25 08:06:42 -0400

    Removed plugins and fixed grml-config.sh

--- grml-config.sh
+++ grml-config.sh
@@ -8,8 +8,3 @@ wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 # optional:
 wget -O .zshrc.local        http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
 
-wget -O .screenrc     http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
-wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc
-wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-# optional:
-wget -O .zshrc.local        http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
